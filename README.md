24fa41ab2608b4785b5644bdf177d58d6402e5ea
ddulh <ddulhddul@gmail.com>
2017-02-25 11:40:51 +0900

    url img 뽑아오기 temp

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-README.md
+## 
+    https://www.google.co.kr/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=nodejs+%ED%81%AC%EB%A1%A4%EB%A7%81
+    https://dobest.io/nodejs-web-crawling-with-cheerio/
+    http://creativeworks.tistory.com/entry/PYTHON-3-Tutorials-24-%EC%9B%B9-%ED%81%AC%EB%A1%A4%EB%9F%AClike-Google-%EB%A7%8C%EB%93%A4%EA%B8%B0-1-How-to-build-a-web
+## crawler
+    http://mititch.tistory.com/36
+
+## Crawling, Scraping, Parsing
+    Scraping - http, https, request
+    Parsing - JSDOM, cheerio
+
+    $ npm install request
+    $ npm install cheerio
+
+## https://www.npmjs.com/package/google-scraper
+    $ npm install google-scraper --save
+    $ npm install urlencode --save
+
+## https://www.npmjs.com/package/google-images-scraper
+    $ npm install google-images-scraper --save
+
+## 프로미스
+    http://programmingsummaries.tistory.com/325
--- crawlServer.js
+++ crawlServer.js
@@ -1,35 +1,49 @@
-/*
-https://www.google.co.kr/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=nodejs+%ED%81%AC%EB%A1%A4%EB%A7%81
-https://dobest.io/nodejs-web-crawling-with-cheerio/
-http://creativeworks.tistory.com/entry/PYTHON-3-Tutorials-24-%EC%9B%B9-%ED%81%AC%EB%A1%A4%EB%9F%AClike-Google-%EB%A7%8C%EB%93%A4%EA%B8%B0-1-How-to-build-a-web-crawler
-http://mititch.tistory.com/36
-
-Crawling, Scraping, Parsing
-	Scraping - http, https, request
-	Parsing - JSDOM, cheerio
-
-$ npm install request
-$ npm install cheerio
-
-https://www.npmjs.com/package/google-scraper
-$ npm install google-scraper --save
-$ npm install urlencode --save
-
-https://www.npmjs.com/package/google-images-scraper
-$ npm install google-images-scraper --save
-
-*/
 var express = require('express');
 var GoogleScraper = require('google-scraper');
 var urlencode = require('urlencode');
+var request = require('request');
+var tidy = require('htmltidy').tidy;
+var cheerio = require('cheerio');
+var jade = require('jade');
 
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', './');
 app.listen('3003', function(){
 	console.log('3003 connected...')
 })
 
+var urlRequest = function (param) {
+	return new Promise(function (resolve, reject) {
+		var result = [];
+		request(url, function (error, response, body) {
+			if (error) throw error;
+			var $ = cheerio.load(body);
+			var postElements = $("img");
+			postElements.each(function(i, obj) {
+				var src = obj.attribs.src;
+	  			if(src.indexOf('http://') != -1){
+	  				result.push(src);
+	  			}
+			});
+			resolve(result);
+		})
+	});
+};
+
+
 app.get('/scrape', function(req,res){
-	var keyword = urlencode('avcc');
+	url = 'http://bikamp.tistory.com/75';
+	urlRequest(url)
+	.then(function (result) {
+		res.render('main', {
+			srcs: result
+		});
+	}, function (error) {
+		res.send('Error'+error);
+	});
+
+	/*var keyword = urlencode('avcc');
 	var scrape = new GoogleScraper({
 	  keyword: keyword,
 	  language: "kr",
@@ -42,17 +56,7 @@ app.get('/scrape', function(req,res){
 	}).catch(function(e) {
 		console.log(222);
 		res.send(e);
-	})
+	})*/
+
 })
 
-/*var request = require('request');
-var tidy = require('htmltidy').tidy;
-var url = 'https://maps.google.co.kr/maps?q=%EC%84%B1%EC%A0%95%EB%8F%99+%EB%A7%9B%EC%A7%91&ion=1&espv=2&bav=on.2,or.r_cp.&bvm=bv.146094739,d.dGo&biw=1280&bih=670&dpr=1&um=1&ie=UTF-8&sa=X&ved=0ahUKEwicyo3Z7PfRAhUDUZQKHQPbCRIQ_AUIBigB';
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  	console.log('########################\n\n\n\n');
-	tidy(body, function(err, html) {
-	    console.log(html);
-	});
-  }
-})*/
