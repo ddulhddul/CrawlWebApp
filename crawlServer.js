2ff416b2f62dee1b20db846e14e61e90891b0831
ddulh <ddulhddul@gmail.com>
2017-02-26 10:16:59 +0900

    재귀호출을 통한 url Request 함수 수정

--- crawlServer.js
+++ crawlServer.js
@@ -13,50 +13,59 @@ app.listen('3003', function(){
 	console.log('3003 connected...')
 })
 
-var urlRequest = function (param) {
+var urlRequest = function (param, i, result) {
 	return new Promise(function (resolve, reject) {
-		var result = [];
-		request(url, function (error, response, body) {
-			if (error) throw error;
-			var $ = cheerio.load(body);
-			var postElements = $("img");
-			postElements.each(function(i, obj) {
-				var src = obj.attribs.src;
-	  			if(src.indexOf('http://') != -1){
-	  				result.push(src);
-	  			}
-			});
-			resolve(result);
-		})
+		
+		console.log('url :::', param[i])
+		if(param[i]){
+			request(param[i], function (error, response, body) {
+				if (error) reject('Unexpected Error :::');
+				var $ = cheerio.load(body);
+				var postElements = $("img");
+				postElements.each(function(i, obj) {
+					var src = obj.attribs.src;
+					if(src){
+			  			if(src.indexOf('http://') != -1){
+			  				result.push(src);
+			  			}
+					}
+				});
+				
+				urlRequest(param, i+1, result)
+				.then(function (r) {
+					resolve(r);
+				}, function (error) {
+					res.send('Error'+error);
+				});
+			})
+		}else resolve(result);
 	});
 };
 
 
 app.get('/scrape', function(req,res){
-	url = 'http://bikamp.tistory.com/75';
-	urlRequest(url)
-	.then(function (result) {
-		res.render('main', {
-			srcs: result
-		});
-	}, function (error) {
-		res.send('Error'+error);
-	});
-
-	/*var keyword = urlencode('avcc');
+	
+	var keyword = urlencode('하하호호');
 	var scrape = new GoogleScraper({
 	  keyword: keyword,
 	  language: "kr",
 	  tld:"com",
-	  results: 100
+	  results: 10
 	}); 
 	scrape.getGoogleLinks.then(function(value) {
-		console.log(111);
-		res.send(value);
+		console.log('GOOGLE SEARCH SUCCESS :::');
+		urlRequest(value, 0, [])
+		.then(function (result) {
+			res.render('main', {
+				srcs: result
+			});
+		}, function (error) {
+			res.send('url request Call Erro :::\n'+error);
+		});
 	}).catch(function(e) {
-		console.log(222);
+		console.log('GOOGLE SEARCH ERROR :::');
 		res.send(e);
-	})*/
+	})
 
 })
 
